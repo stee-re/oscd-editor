650a0194de052b022afd1fef9abfdb5e9559f511
Christian Dinkel <christian.dinkel@omicronenergy.com>
2025-04-04 10:54:13 +0200

    chore: remove superfluous dependencies

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,18 +1,4 @@
-const filteredLogs = ["in dev mode", "scheduled an update", "Multiple versions of Lit"];
-
 export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
-  filterBrowserLogs(log) {
-    for (const arg of log.args) {
-      if (
-        typeof arg === "string" &&
-        filteredLogs.some((l) => arg.includes(l))
-      ) {
-        return false;
-      }
-    }
-    return true;
-  },
-
   /** Test files to run */
   files: ["dist/**/*.spec.js", "!node_modules/**/*"],
 
@@ -24,22 +10,4 @@ export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
   coverageConfig: {
     exclude: ["testHelpers.ts", "node_modules/**/*"],
   },
-
-  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto',
-
-  /** Amount of browsers to run concurrently */
-  // concurrentBrowsers: 2,
-
-  /** Amount of test files per browser to test concurrently */
-  // concurrency: 1,
-
-  /** Browsers to run tests on */
-  // browsers: [
-  //   playwrightLauncher({ product: 'chromium' }),
-  //   playwrightLauncher({ product: 'firefox' }),
-  //   playwrightLauncher({ product: 'webkit' }),
-  // ],
-
-  // See documentation for all available options
 });
